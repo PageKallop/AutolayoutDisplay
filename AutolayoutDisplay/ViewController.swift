2560f039674218306238bb5dd7de6d40a5047056
PageKallop <PageMarie78@gmail.com>
2021-02-16 20:52:24 -0600

    Added text view

--- AutolayoutDisplay/ViewController.swift
+++ AutolayoutDisplay/ViewController.swift
@@ -8,24 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let pawImage: UIImageView = {
+        let imageView = UIImageView(image: #imageLiteral(resourceName: "dogprint"))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    let descriptionTextView: UITextView = {
+        
+        let textView = UITextView()
+        textView.text = "Join us today in our fun and games!"
+        textView.translatesAutoresizingMaskIntoConstraints = false 
+        return textView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+   
+       
+        view.addSubview(pawImage)
+        view.addSubview(descriptionTextView)
+        setUpLayout()
+      
         
-        view.backgroundColor = .systemPink
- 
-        let imageView = UIImageView(image: #imageLiteral(resourceName: "dogprint"))
-        view.addSubview(imageView)
-        //imageView.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
-        //enables outolayout for image 
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        
-        imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        imageView.topAnchor.constraint(equalTo:view.topAnchor, constant: 100).isActive = true
-        imageView.widthAnchor.constraint(equalToConstant: 200).isActive = true
-        imageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
     }
-
+    
+    private func setUpLayout() {
+        //image constraints
+        pawImage.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        pawImage.topAnchor.constraint(equalTo:view.topAnchor, constant: 100).isActive = true
+        pawImage.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        pawImage.heightAnchor.constraint(equalToConstant: 200).isActive = true
+       
+        //text description constraints
+        descriptionTextView.topAnchor.constraint(equalTo: pawImage.bottomAnchor, constant: 150).isActive = true
+        descriptionTextView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        descriptionTextView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        descriptionTextView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
+        
+    }
 
 }
 
